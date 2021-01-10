b2a6edb63fd0e2f8637783e9806dbdf5765257a3
Yuval-Hoshea <77237873+Yuval-Hoshea@users.noreply.github.com>
2021-01-10 23:16:18 +0200

    Update url.js

--- static/js/url.js
+++ static/js/url.js
@@ -3,5 +3,5 @@ const url =
     "hey": "hello",
     "ip": "0.0.0.0",
     "port": "12345",
-    "url": "https://flask-chat-yuval-hoshea.herokuapp.com"
+    "url": "https://flask-chat-yuval-hoshea.herokuapp.com/chat"
 }
